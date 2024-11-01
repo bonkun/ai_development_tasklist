ade19ed4a11b758036e46ddaf77953b08c03ede5
松本健 <matsumotoken@matsumotokennoMacBook-Pro.local>
2024-11-01 20:54:25 +0900

    dump file all table

--- myapp_2024-11-01.sql
+++ myapp_2024-11-01.sql
@@ -7,7 +7,7 @@
 #
 # ホスト: localhost (MySQL 8.0.39)
 # データベース: myapp
-# 生成時間: 2024-11-01 11:51:22 +0000
+# 生成時間: 2024-11-01 11:54:05 +0000
 # ************************************************************
 
 
@@ -42,6 +42,38 @@ VALUES
 	(4,'保留');
 
 /*!40000 ALTER TABLE `task_progress` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# テーブルのダンプ tasklist
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tasklist`;
+
+CREATE TABLE `tasklist` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `content` varchar(2000) DEFAULT NULL,
+  `due` datetime DEFAULT NULL,
+  `priority` int DEFAULT NULL,
+  `progress_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+LOCK TABLES `tasklist` WRITE;
+/*!40000 ALTER TABLE `tasklist` DISABLE KEYS */;
+
+INSERT INTO `tasklist` (`id`, `title`, `content`, `due`, `priority`, `progress_id`)
+VALUES
+	(2,'更新できたかな2','内容2','2024-02-24 15:30:00',2,1),
+	(3,'更新できたかな3','内容3','2024-02-25 09:00:00',3,2),
+	(4,'caww22t','内容1','2024-02-23 12:00:00',1,1),
+	(5,'doww22g','内容2','2024-02-24 15:30:00',2,1),
+	(6,'更新できたかな3','内容3','2024-02-25 09:00:00',3,2),
+	(21,'インサートテスト2','いくぜ','2023-12-31 23:59:59',1,1),
+	(22,'インサートテスト3','いくぜ','2024-01-15 12:00:00',2,1);
+
+/*!40000 ALTER TABLE `tasklist` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
